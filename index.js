a3803b294bb345f03ee8e866b81e94658ae6d669
Paras <paras.starkmarkup@gmail.com>
2025-06-28 14:03:01 +0530

    new changes

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const mapsRoutes = require('./routes/maps');
 const app = express();
 app.use(cors(
   {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://pathixfrontend.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   }
 ));
 
