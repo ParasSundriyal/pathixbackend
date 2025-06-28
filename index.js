1fad1e49a20d350fa16b771d25b40f6398716e13
Paras <paras.starkmarkup@gmail.com>
2025-06-28 15:14:44 +0530

    new changes4

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors(
   {
     origin: ['http://localhost:3000', 'https://pathixfrontend.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }
